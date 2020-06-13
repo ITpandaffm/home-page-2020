8320da6cd5651104aaffcf8bd2913b5b0ec83b97
ffm <fengfuming2007@126.com>
2020-06-13 22:37:58 +0800

    feat: 初版

--- src/App.js
+++ src/App.js
@@ -3,24 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello ffm</h1>
+                <p className={'contact'}>fuming.ffm@alibaba-inc.com</p>
+            </header>
+            <video
+                autoPlay={true}
+                loop={true}
+                muted={true}
+                src="http://cdn.fengfuming.com/coverr-aerial-view-of-rocky-mountains-1585320758193.mp4"/>
+        </div>
+    );
 }
 
 export default App;
